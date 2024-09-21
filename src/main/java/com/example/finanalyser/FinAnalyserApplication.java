02c519205fdf1b1401e6b53d6234f464cd924c83
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-09-21 12:53:36 +0200

    created

---
+++ src/main/java/com/example/finanalyser/FinAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.example.finanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinAnalyserApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( FinAnalyserApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/example/finanalyser/FinAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinAnalyserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
